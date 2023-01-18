1d3a2bf1214936850c1b88f1cba9a3aa87522ebb
Martín <magadiflo@gmail.com>
2023-01-17 20:39:03 -0500

    Creando componentes - CLI Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MyComponent1Component } from './my-component1/my-component1.component';
+import { MyComponent2Component } from './my-component2/my-component2.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent1Component,
+    MyComponent2Component
   ],
   imports: [
     BrowserModule,
