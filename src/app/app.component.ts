ddd397e375ec90b44356ae061be3e806d46d56a6
Martín <magadiflo@gmail.com>
2023-03-22 17:18:28 -0500

    Ciclo de vida de componentes - 1° Constructor(), 2° ngOnChanges()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'curso-angular';
+
+  constructor() {
+    console.log('1° app.component -> constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('2° app.component -> ngOnChanges');
+  }
 
   getMessage(value: string): void {
     console.log(value);
+  }
+
+  clickChangeTitle(): void {
+    this.title = 'Título cambiado!!!';
   }
 }
--- src/app/my-component1/my-component1.component.ts
+++ src/app/my-component1/my-component1.component.ts
@@ -1,20 +1,26 @@
-import { Component, Input, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ViewChild, ElementRef, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-my-component1',
   templateUrl: './my-component1.component.html',
   styleUrls: ['./my-component1.component.scss']
 })
-export class MyComponent1Component implements OnInit {
+export class MyComponent1Component implements OnInit, OnChanges {
 
   @Input() title = '';
   @Output() sendMessage = new EventEmitter<string>();
   @ViewChild('inputText') inputText: ElementRef | undefined;
 
   constructor() { }
-
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('2° my-component1.component -> ngOnChanges [Solo funciona cuando hay una propiedad decorada con @Input()]');
+    console.log(changes);
+  }
+  
   ngOnInit(): void {
   }
+
 
   clickSendMessageToFather(): void {
     this.sendMessage.emit(this.inputText?.nativeElement.value);
