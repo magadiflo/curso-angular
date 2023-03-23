179b4a11c4cc3280219032a254d5b266b2226e5d
Martín <magadiflo@gmail.com>
2023-03-23 10:42:24 -0500

    Ciclo de vida de componentes - 4° - 3° ngAfterViewInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import { Component, OnInit, DoCheck, ViewChild, AfterViewInit } from '@angular/core';
+import { MyComponent2Component } from './my-component2/my-component2.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, DoCheck {
+export class AppComponent implements OnInit, DoCheck, AfterViewInit {
   title = 'curso-angular';
   show = false;
+
+  @ViewChild(MyComponent2Component) viewChild: MyComponent2Component | undefined;
 
   constructor() {
     // console.log('1° app.component -> constructor');
   }
 
   ngOnInit(): void {
-    // console.log('3° app.component -> ngOnInit');
+    console.log('3° app.component -> ngOnInit');
+    console.log(this.viewChild);
   }
 
   ngDoCheck(): void {
-    // console.log('4° app.component -> ngDoCheck');
+    console.log('4° app.component -> ngDoCheck');
+    console.log(this.viewChild);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('4° - 3° app.component -> ngAfterViewInit');
+    console.log(this.viewChild);
   }
 
   getMessage(value: string): void {
